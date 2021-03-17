a70579089244a6f84a1a084ca8bbd887a731b973
Vlad Rossohin <rossohin@auris.rfe.bsu.by>
2021-03-17 12:28:27 +0300

    implement part 1

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -15,18 +15,18 @@
 #include <cmath>
 
 using namespace std;
-int value = 0;
-MPI_Status status;
-int countOfProcesses, currentProcess;
 
-void sendValue(int value);
-void receiveValue(int receiver);
+int countOfProcesses;
+const int sender = 6;
+const int receiver = 3;
+
+bool ifProcessWillBeScipped(int countOfSkippedProcesses, int indexOfProcess);
 
 int main(int* argc, char** argv)
 {
-	int sender = 0;		
-	int receiver = 1;	
-	
+	int value = 0;
+	MPI_Status status;
+	int currentProcess;
 	if (sender == receiver) {
 		cout << "You cannot send value by some process to itself!" << endl;
 		return 0;
@@ -36,58 +36,63 @@ int main(int* argc, char** argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &currentProcess);
 	MPI_Comm_size(MPI_COMM_WORLD, &countOfProcesses);
 
-	if (countOfProcesses - 1 <= sender || countOfProcesses - 1 <= receiver) {
+	const int countOfSkippedProcesses = receiver > sender ?
+		countOfProcesses - (receiver - sender) - 1 :
+		(sender - receiver) - 1;
+
+	if (countOfProcesses - 1 < sender || countOfProcesses - 1 < receiver) {
 		if (sender > receiver) {
-		cout << "Please select a count of processes greater than " << sender + 1 << endl;
+		cout << "Please select a count of processes greater than " << sender << endl;
 		}
 		else {
-			cout << "Please select a count of processes greater than " << receiver + 1 << endl;
+			cout << "Please select a count of processes greater than " << receiver << endl;
 		}
 		return 0;
 	}
 
-	int value = 13;
-	//MPI_Status status;
-
 	// PART 1: from one to another
-	/*if (currentProcess == sender) {
-		sendValue(receiver, value);
-	} 
-	else {			
-		receiveValue(receiver);
-	}*/
+	value = 4;
+	if (currentProcess == sender) {
+		MPI_Send(&value, 1, MPI_INT, (currentProcess == countOfProcesses - 1) ? 0 : currentProcess + 1, 0, MPI_COMM_WORLD);
+	}
+	else {
+		if (!ifProcessWillBeScipped(countOfSkippedProcesses, currentProcess)) {
+			MPI_Recv(&value, 1, MPI_INT, (currentProcess == 0) ? countOfProcesses - 1 : currentProcess - 1, 0, MPI_COMM_WORLD, &status);
+			cout << "Process " << currentProcess << " got " << value << endl;
+
+			if (currentProcess != receiver && !ifProcessWillBeScipped(countOfSkippedProcesses, (currentProcess == countOfProcesses - 1) ? 0 : currentProcess + 1)) {
+				MPI_Send(&value, 1, MPI_INT, (currentProcess == countOfProcesses - 1) ? 0 : currentProcess + 1, 0, MPI_COMM_WORLD);
+			}
+
+		}
+	}
 	
 
 	//PART 2: form all to all
-	value = currentProcess;
+	//value = currentProcess;
 
-	sendValue(currentProcess);
+	//currentProcess == 0 ? receiveValue(countOfProcesses - 1) : receiveValue(currentProcess - 1);
+	//sendValue(currentProcess);
 
-	for (int i = 0; i < countOfProcesses; i++) {
-		if (i == currentProcess) {
-			continue;
-		}
-		receiveValue(i);
-	}
+	///*for (int i = 0; i < countOfProcesses; i++) {
+	//	if (i == currentProcess) {
+	//		continue;
+	//	}
+	//}
+	//*/	
 
-	cout << "Process " << currentProcess << " got " << value << endl;
 
 	MPI_Finalize();
 }
 
-void sendValue(int value) {
-	if (currentProcess == countOfProcesses - 1) {
-		MPI_Send(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+bool ifProcessWillBeScipped(int countOfSkippedProcesses, int indexOfProcess)
+{
+	if (countOfSkippedProcesses != 0) {
+		for (int i = 1; i <= countOfSkippedProcesses; i++) {
+			if (indexOfProcess == (receiver + i) % countOfProcesses) {
+				return true;
+			}
+		}
 	}
-	else {
-		MPI_Send(&value, 1, MPI_INT, currentProcess + 1, 0, MPI_COMM_WORLD);
-	}
-}
-
-void receiveValue(int receiver) {
-	MPI_Recv(&value, 1, MPI_INT, currentProcess == 0 ? countOfProcesses - 1 : currentProcess - 1, 0, MPI_COMM_WORLD, &status);
-
-	if (currentProcess != receiver) {
-		MPI_Send(&value, 1, MPI_INT, currentProcess == countOfProcesses - 1 ? 0 : currentProcess + 1, 0, MPI_COMM_WORLD);
-	}
+	return false;
 }
